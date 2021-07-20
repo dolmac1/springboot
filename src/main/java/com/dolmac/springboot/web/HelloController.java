7ea25cae8044ddaf903c2e1a61079b5c80b0aecd
dolmac <dolmac@naver.com>
2021-07-21 00:05:28 +0900

    helloDto도 테스트

--- src/main/java/com/dolmac/springboot/web/HelloController.java
+++ src/main/java/com/dolmac/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.dolmac.springboot.web;
 
 
+import com.dolmac.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //컨트롤러를 json을 반환하는 컨트롤러로 변환해줌
@@ -10,5 +12,8 @@ public class HelloController {
     public String hello(){
         return "hello";
     }// 이제 이 프로젝트는 /hello로 요청이 오면 hello를 반환해줄 것
-
+    @GetMapping("/hello/dto") //HelloResponseDto 를 get 해서 사용
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 }
--- src/test/java/com/dolmac/springboot/HelloControllerTest.java
+++ src/test/java/com/dolmac/springboot/HelloControllerTest.java
@@ -10,9 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 @RunWith(SpringRunner.class) // 실행자를 스프링 부트로 바꾸겠다.
 @WebMvcTest(controllers = HelloController.class) //스프링 테스트 어노테이션 선언
 public class HelloControllerTest {
@@ -26,5 +25,15 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))//MockMvc를 통해 /hello를 get요청
                 .andExpect(status().isOk())//mvc.perform의 결과를 검증해서 상태가 200인가
                 .andExpect(content().string(hello));//리턴하는 값이 string type의 hello가 맞는지 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "dolmac";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))//param 은 파라미터 지정(단 string만 가능해서 변환)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))//선언된 필드와 받아온 필드가 값이 같은지 비교
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
